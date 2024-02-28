cc296a37e2358b841f917dde66141427334230e3
yukino <2651518588@qq.com>
2024-02-28 22:59:15 +0800

    登陆验证码校验开关实现

--- lihua-admin/src/main/java/com/lihua/system/controller/SysAuthenticationController.java
+++ lihua-admin/src/main/java/com/lihua/system/controller/SysAuthenticationController.java
@@ -3,6 +3,7 @@ package com.lihua.system.controller;
 import com.anji.captcha.model.common.ResponseModel;
 import com.anji.captcha.model.vo.CaptchaVO;
 import com.anji.captcha.service.CaptchaService;
+import com.lihua.config.LihuaConfig;
 import com.lihua.enums.ResultCodeEnum;
 import com.lihua.model.web.ControllerResult;
 import com.lihua.model.security.SysUserVO;
@@ -19,16 +20,20 @@ public class SysAuthenticationController extends ControllerResult {
     @Resource
     private CaptchaService captchaService;
 
+    @Resource
+    private LihuaConfig lihuaConfig;
+
     @PostMapping("login")
     public String login(@RequestBody SysUserVO sysUserVO,String captchaVerification) {
-        CaptchaVO captchaVO = new CaptchaVO();
-        captchaVO.setCaptchaVerification(captchaVerification);
-        ResponseModel verification = captchaService.verification(captchaVO);
-
-        if (verification.isSuccess()) {
-            return success(sysAuthenticationService.login(sysUserVO));
+        // 开启验证码情况下进行验证
+        if (lihuaConfig.getEnableVerificationCode() != null && lihuaConfig.getEnableVerificationCode()) {
+            CaptchaVO captchaVO = new CaptchaVO();
+            captchaVO.setCaptchaVerification(captchaVerification);
+            ResponseModel verificationModel = captchaService.verification(captchaVO);
+            if (!verificationModel.isSuccess()) {
+                return error(ResultCodeEnum.ERROR, verificationModel.getRepMsg());
+            }
         }
-
-        return error(ResultCodeEnum.ERROR,verification.getRepMsg());
+        return success(sysAuthenticationService.login(sysUserVO));
     }
 }
