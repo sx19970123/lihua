078c8e705278c4eb1e8a69b6c0934abef289935f
Yukino <2651518588@qq.com>
2024-05-21 10:42:15 +0800

    暗色模式动画

--- lihua-vue/src/components/light-dark-switch/index.vue
+++ lihua-vue/src/components/light-dark-switch/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div style="padding-right: 16px;">
-    <a-switch v-model:checked="themeStore.dataTheme" @change="themeStore.changeDataDark" :style="{'background': themeStore.dataTheme ? '#00008B' : '#2196F3'}">
+    <a-switch v-model:checked="themeStore.dataTheme"
+              @change="handleChangeTheme"
+              :style="{'background': themeStore.dataTheme ? '#00008B' : '#2196F3'}">
       <template #checkedChildren>
         <Moon/>
       </template>
@@ -14,5 +16,46 @@
 import { useThemeStore } from "@/stores/modules/theme";
 import Sun from "@/components/icon/sun/Sun.vue";
 import Moon from "@/components/icon/moon/Moon.vue";
+import {watch} from "vue";
 const themeStore = useThemeStore()
+
+interface Doc extends Document{
+  startViewTransition: (arg: unknown) => Transition
+}
+
+type Transition = {
+  ready: Promise<void>
+}
+
+
+const handleChangeTheme = (checked: boolean, event: PointerEvent) => {
+    const transition = (document as Doc).startViewTransition(() => {
+      themeStore.changeDataDark()
+    })
+    const {clientX, clientY} = event
+    const endRadius = Math.hypot(
+        Math.max(clientX, innerWidth - clientX),
+        Math.max(clientY, innerHeight - clientY)
+    );
+
+  transition.ready.then(() => {
+      const clipPath = [
+        `circle(0px at ${clientX}px ${clientY}px)`,
+        `circle(${endRadius}px at ${clientX}px ${clientY}px)`,
+      ];
+
+      document.documentElement.animate(
+          {
+            clipPath: checked ? [...clipPath].reverse() : clipPath,
+          },
+          {
+            duration: 400,
+            easing: "ease-in",
+            pseudoElement: checked
+                ? "::view-transition-old(root)"
+                : "::view-transition-new(root)",
+          }
+      );
+    });
+}
 </script>
