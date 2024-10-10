ae09d6139609c837e5eb47060e23f1a005d93ffe
Yukino <2651518588@qq.com>
2024-10-10 22:21:49 +0800

    前端文档菜单

--- lihua-vue/src/views/document/web/DocumentWeb.vue
+++ lihua-vue/src/views/document/web/DocumentWeb.vue
@@ -1,9 +1,30 @@
 <template>
   <a-card>
-    <div class="error-container">
-      <component is="XiaoHeiLookForward" style="font-size: 96px"/>
-      <a-typography-title>前端文档建设中~</a-typography-title>
-    </div>
+    <a-row>
+      <a-col :span="4">
+        <a-menu>
+          <a-menu-item-group title="展示组件">
+            <a-menu-item> 卡片展示</a-menu-item>
+            <a-menu-item> 图标组件</a-menu-item>
+            <a-menu-item> 用户头像</a-menu-item>
+            <a-menu-item> 用户显示</a-menu-item>
+          </a-menu-item-group>
+          <a-menu-item-group title="输入组件">
+            <a-menu-item> 卡片选择</a-menu-item>
+            <a-menu-item> 颜色选择</a-menu-item>
+            <a-menu-item> 部门选择</a-menu-item>
+            <a-menu-item> 富文本编辑器</a-menu-item>
+            <a-menu-item> 图标选择</a-menu-item>
+            <a-menu-item> 密码输入</a-menu-item>
+            <a-menu-item> 用户选择</a-menu-item>
+          </a-menu-item-group>
+          <a-menu-item-group title="工具类">
+            <a-menu-item> 全屏加载</a-menu-item>
+          </a-menu-item-group>
+
+        </a-menu>
+      </a-col>
+    </a-row>
   </a-card>
 </template>
 <style scoped>
