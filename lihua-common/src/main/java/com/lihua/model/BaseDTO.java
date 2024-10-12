49b24bc252943da3aac276ad286c10276d37be28
imlam <burninglam@qq.com>
2024-10-13 00:02:07 +0800

    fix: 分页查询时可能出现的NPE&增加超大分页参数限制

--- lihua-common/src/main/java/com/lihua/model/BaseDTO.java
+++ lihua-common/src/main/java/com/lihua/model/BaseDTO.java
@@ -1,16 +1,48 @@
 package com.lihua.model;
 
-import lombok.Data;
-
+import jakarta.validation.constraints.Max;
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Optional;
+import lombok.Data;
 
 @Data
 public class BaseDTO implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
+    /**
+     * 最大分页页码
+     */
+    public static final int MAX_PAGE_NUM = 100;
+    /**
+     * 最大分页大小
+     */
+    public static final int MAX_PAGE_SIZE = 100;
+    /**
+     * 默认分页页码
+     */
+    public static final int DEFAULT_PAGE_NUM = 1;
+    /**
+     * 默认分页大小
+     */
+    public static final int DEFAULT_PAGE_SIZE = 10;
 
-    private Integer pageNum;
+    /**
+     * 当前页码
+     */
+    @Max(value = MAX_PAGE_NUM, message = "当前分页参数超出限制")
+    protected Integer pageNum;
 
-    private Integer pageSize;
+    /**
+     * 当前分页大小
+     */
+    @Max(value = MAX_PAGE_SIZE, message = "当前分页参数超出限制")
+    protected Integer pageSize;
+
+    //在构造方法中设置默认值
+    public BaseDTO() {
+        this.pageNum = Optional.ofNullable(pageNum).orElse(BaseDTO.DEFAULT_PAGE_NUM);
+        this.pageSize = Optional.ofNullable(pageSize).orElse(BaseDTO.DEFAULT_PAGE_SIZE);
+    }
 }
