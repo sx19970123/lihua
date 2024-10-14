e103e02ce2b2970fe249c7985de1f8f12611a966
Yukino <2651518588@qq.com>
2024-10-14 08:44:15 +0800

    设置keep-alive最大数

--- lihua-vue/src/layout/content/index.vue
+++ lihua-vue/src/layout/content/index.vue
@@ -3,7 +3,7 @@
     <router-view v-slot="{ Component, route}">
       <!-- zoom： 变焦 fade：淡入淡出 breathe：呼吸 top：上升 down：切换 switch：交换 trick：整活  -->
       <transition :name="themeStore.routeTransition" mode="out-in">
-        <keep-alive :include = viewTabsStore.$state.componentAlive>
+        <keep-alive :include = "viewTabsStore.$state.componentAlive" :max="5">
           <component :is="Component" :key="route.path + viewTabsStore.$state.contentComponentKey"/>
         </keep-alive>
       </transition>
