f9fcbe7b67ac2e7e30e045adc0df2dbe91b77a75
Yukino <2651518588@qq.com>
2025-04-07 23:39:39 +0800

    table-setting demo

---
+++ lihua-vue/src/views/component/table-setting/Demo1.vue
@@ -0,0 +1,43 @@
+<template>
+  <!--            表格设置-->
+  <table-setting v-model="columns"/>
+  <a-table :dataSource="dataSource" :columns="columns" />
+</template>
+<script setup lang="ts">
+import TableSetting from "@/components/table-setting/index.vue";
+import {ref} from "vue";
+import type { ColumnsType } from 'ant-design-vue/es/table/interface';
+
+const columns = ref<ColumnsType>([
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+])
+
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+]
+</script>
---
+++ lihua-vue/src/views/component/table-setting/TableSetting.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-card>
+    <template #title>
+      <a-typography-link :href="baseDocApi + '/document/component/SELECTABLE-CARD'" target="_blank">组件文档</a-typography-link>
+    </template>
+    <a-flex vertical :gap="16" style="margin-top: 16px">
+      <demo1/>
+      <demo2/>
+      <demo3/>
+      <demo4/>
+    </a-flex>
+  </a-card>
+</template>
+
+<script setup lang="ts">
+import Demo1 from './Demo1.vue'
+import Demo2 from './Demo2.vue'
+import Demo3 from './Demo3.vue'
+import Demo4 from './Demo4.vue'
+const baseDocApi = import.meta.env.VITE_APP_DOC_API
+</script>
