3760b58b3046f06fadccb1020d5abc228d4f2077
Yukino <2651518588@qq.com>
2025-12-10 16:02:24 +0800

    app 通知公告接口

---
+++ lihua-admin/src/main/java/com/lihua/controller/app/AppSysNoticeController.java
@@ -0,0 +1,61 @@
+package com.lihua.controller.app;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.lihua.model.system.dto.SysNoticeDTO;
+import com.lihua.model.system.vo.SysNoticeVO;
+import com.lihua.model.system.vo.SysUserNoticeVO;
+import com.lihua.model.web.ApiResponseModel;
+import com.lihua.model.web.basecontroller.ApiResponseController;
+import com.lihua.service.system.notice.SysNoticeService;
+import com.lihua.service.system.user.SysUserNoticeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "通知公告")
+@RestController
+@RequestMapping("app/system/notice")
+@Validated
+public class AppSysNoticeController extends ApiResponseController {
+
+    @Resource
+    private SysNoticeService sysNoticeService;
+
+    @Resource
+    private SysUserNoticeService sysUserNoticeService;
+
+    @Operation(summary = "预览通知公告")
+    @GetMapping("preview/{id}")
+    public ApiResponseModel<SysNoticeVO> preview(@PathVariable("id") String id) {
+        return success(sysNoticeService.preview(id));
+    }
+
+
+    @Operation(summary = "用户查询消息通知")
+    @PostMapping("list/{userId}")
+    public ApiResponseModel<IPage<SysUserNoticeVO>> queryListByUserId(@PathVariable("userId") String userId, @RequestBody SysNoticeDTO sysNoticeDTO) {
+        return success(sysNoticeService.queryListByUserId(userId, sysNoticeDTO));
+    }
+
+    @Operation(summary = "用户添加star")
+    @PostMapping("star/{noticeId}/{star}")
+    public ApiResponseModel<String> changeStar(@PathVariable("noticeId") String noticeId, @PathVariable("star") String star) {
+        sysUserNoticeService.changeStar(noticeId, star);
+        return success();
+    }
+
+    @Operation(summary = "用户已读")
+    @PostMapping("read/{noticeId}")
+    public ApiResponseModel<String> changeRead(@PathVariable("noticeId") String noticeId) {
+        sysUserNoticeService.changeRead(noticeId);
+        return success();
+    }
+
+    @Operation(summary = "获取未读消息总数")
+    @GetMapping("unread/count")
+    public ApiResponseModel<Integer> queryUnReadCount() {
+        return success(sysUserNoticeService.queryUnReadCount());
+    }
+}
