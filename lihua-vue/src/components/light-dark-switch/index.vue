6d7dd7b0c788098a842ee164d9a1abb501b01f81
Yukino <2651518588@qq.com>
2026-01-05 14:21:21 +0800

    修复暗色模式刷新后失效的bug

--- lihua-vue/src/components/light-dark-switch/index.vue
+++ lihua-vue/src/components/light-dark-switch/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding-right: var(--lihua-space-base)">
     <a-config-provider :wave="{ disabled: true }">
-      <a-switch v-model:checked="themeStore.isDarkTheme" @change="themeStore.changeDataDark" :style="{'background': themeStore.isDarkTheme ? '#00008B' : '#2196F3'}">
+      <a-switch v-model:checked="themeStore.isDarkTheme" @change="themeStore.changeDataDark()" :style="{'background': themeStore.isDarkTheme ? '#00008B' : '#2196F3'}">
         <template #checkedChildren>
           <XiaoHeiMoon/>
         </template>
