3bbbe6167af7c0a2271ff7425cd7729b28170b56
Yukino <2651518588@qq.com>
2026-03-08 22:56:33 +0800

    security 异常处理

--- lihua-base/lihua-security/src/main/java/com/lihua/filter/JwtAuthenticationTokenFilter.java
+++ lihua-base/lihua-security/src/main/java/com/lihua/filter/JwtAuthenticationTokenFilter.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal( HttpServletRequest request,  HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 获取token
         String token = TokenUtils.getToken(request);
 
