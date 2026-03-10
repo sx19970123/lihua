efc6f375607cd136ba73c664e018ba4b1324aca5
Yukino <2651518588@qq.com>
2026-03-10 20:16:02 +0800

    去除登录加密

--- lihua-base/lihua-common/src/main/java/com/lihua/utils/crypt/AesUtils.java
+++ lihua-base/lihua-common/src/main/java/com/lihua/utils/crypt/AesUtils.java
@@ -4,8 +4,6 @@ import lombok.SneakyThrows;
 import org.springframework.util.StringUtils;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
