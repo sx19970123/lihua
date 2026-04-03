b56a628e96360bf1e1719ab269ec5ebc087e9dab
Yukino <2651518588@qq.com>
2026-04-03 14:05:13 +0800

    面包屑显示/隐藏增加动画效果

--- lihua-vue/src/layout/head/components/breadcrumb/index.vue
+++ lihua-vue/src/layout/head/components/breadcrumb/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <div>
+  <transition :name="theme.$state.routeTransition" mode="out-in">
     <a-breadcrumb :routes="pageRoute" v-if="pageRoute && pageRoute.length > 1 && pageRoute[0].path !== ''">
       <template #itemRender="{route}">
         {{ route.meta.label }}
       </template>
     </a-breadcrumb>
-  </div>
+  </transition>
 </template>
 <script lang="ts" setup>
 import {type RouteRecordRaw, useRoute} from "vue-router";
 import {ref, watch} from "vue";
-
+import {useThemeStore} from "@/stores/theme.ts";
+const theme = useThemeStore();
 const route = useRoute()
 const pageRoute = ref<RouteRecordRaw[]>();
 if (route.fullPath !== '/index') {
