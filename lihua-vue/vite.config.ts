9d79d529cf9201d7446b6fb6a7a2216dc796fc49
Yukino <2651518588@qq.com>
2026-04-28 21:14:07 +0800

    web端同步

--- lihua-vue/vite.config.ts
+++ lihua-vue/vite.config.ts
@@ -2,7 +2,7 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-import replaceAttrFill from "./plugins/SvgoPlugin.ts"
+import replaceAttrFill from "./plugins/svgo-plugin.ts"
 
 export default defineConfig(({ mode }) => {
   // 获取请求前缀
